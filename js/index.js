f288177590008b67cda27bda267bdd85ce02287f
Andrei Dzebysh <presnia@yahoo.com>
2021-07-06 10:35:58 +0300

    Added possibility to open and close modal window
    Corrected behavior of window scroll for all browsers

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const headerCityButton = document.querySelector('.header__city-button');
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
 
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
@@ -6,4 +8,47 @@ headerCityButton.addEventListener('click', () => {
   const city = prompt('Укажите ваш город');
   headerCityButton.textContent = city;
   localStorage.setItem('lomoda-location', city);
+});
+
+const disableScroll = () => {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.body.dbScrollY = wimdow.scrollY;
+
+  document.body.style.cssText = `
+    position: fixed;
+    left: 0;
+    top: ${window.scrollY}px;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+  `;
+}
+
+const enableScroll = () => {
+  document.body.style.cssText = '';
+  window.scroll({
+    top: document.body.dbScrollY,
+  });
+}
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+  disableScroll();
+}
+
+cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+  enableScroll();
+}
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', e => {
+  const target = e.target;
+  
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
 });
